575521102ce53a3f8eff7bbec7abcfd89a41c474
Sahil <leviack629@gmail.com>
2023-03-26 15:48:12 +0530

    intro

---
+++ class1/intro.js
@@ -0,0 +1,4 @@
+var user = { name: "Sahil", age: 22 };
+console.log("Sahil");
+console.log(user.name);
+console.log(user.email);
---
+++ class1/intro.ts
@@ -0,0 +1,6 @@
+let user = {name: "Sahil", age: 22}
+
+console.log("Sahil")
+console.log(user.name)
+console.log(user.email)
+
